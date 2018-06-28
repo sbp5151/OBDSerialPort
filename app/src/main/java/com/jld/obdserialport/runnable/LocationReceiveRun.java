a52de9b8c345546124e4a141d363055a1b6dc5dd
sbp5151 <sbp5151@163.com>
2018-06-28 15:13:04 +0800

    调试修改

--- app/src/main/java/com/jld/obdserialport/runnable/LocationReceiveRun.java
+++ app/src/main/java/com/jld/obdserialport/runnable/LocationReceiveRun.java
@@ -14,7 +14,10 @@ import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 
 import com.jld.obdserialport.bean.GPSBean;
+import com.jld.obdserialport.event_msg.OBDDataMessage;
 import com.jld.obdserialport.http.GPSHttpUtil;
+
+import org.greenrobot.eventbus.EventBus;
 
 import java.util.List;
 
@@ -27,10 +30,13 @@ public class LocationReceiveRun {
     private Context mContext;
     private LocationManager mLocationManager;
     public static GPSBean mGpsBean = new GPSBean();
+    private final EventBus mEventBus;
 
     public LocationReceiveRun(Context context) {
         mContext = context;
         Log.d(TAG, "LocationReceiveRun");
+        mEventBus = EventBus.getDefault();
+        mEventBus.post(new OBDDataMessage(OBDDataMessage.CONTENT_FLAG, "LocationReceiveRun"));
         initLocation();
     }
 
@@ -42,6 +48,7 @@ public class LocationReceiveRun {
         if (ActivityCompat.checkSelfPermission(mContext, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
                 && ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             Log.d(TAG, "LocationReceiveRun 权限错误");
+            mEventBus.post(new OBDDataMessage(OBDDataMessage.CONTENT_FLAG, "LocationReceiveRun 权限错误"));
             return;
         }
         List<String> allProviders = mLocationManager.getAllProviders();
@@ -51,11 +58,15 @@ public class LocationReceiveRun {
         if (mLocationManager.getProvider(LocationManager.GPS_PROVIDER) != null) {
             mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 3000, 3, mLocationListener);
             Log.d(TAG, "GPS定位...");
+            mEventBus.post(new OBDDataMessage(OBDDataMessage.CONTENT_FLAG, "LocationReceiveRun GPS定位"));
+
         } else if (mLocationManager.getProvider(LocationManager.NETWORK_PROVIDER) != null) {
             mLocationManager.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, 3000, 3, mLocationListener);
+            mEventBus.post(new OBDDataMessage(OBDDataMessage.CONTENT_FLAG, "LocationReceiveRun 网络定位"));
             Log.d(TAG, "网络定位...");
         } else if (mLocationManager.getProvider(LocationManager.PASSIVE_PROVIDER) != null) {
             mLocationManager.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, 3000, 3, mLocationListener);
+            mEventBus.post(new OBDDataMessage(OBDDataMessage.CONTENT_FLAG, "LocationReceiveRun 其他定位"));
             Log.d(TAG, "其他定位...");
         }
     }
@@ -65,6 +76,8 @@ public class LocationReceiveRun {
         @Override
         public void onLocationChanged(Location location) {
             Log.d(TAG, "------------onLocationChanged: " + location);
+            mEventBus.post(new OBDDataMessage(OBDDataMessage.CONTENT_FLAG, "onLocationChanged"));
+
             mGpsBean.setDirection(location.getBearing());
             mGpsBean.setLatitude(location.getLatitude());
             mGpsBean.setLongitude(location.getLongitude());
@@ -73,16 +86,19 @@ public class LocationReceiveRun {
 
         @Override
         public void onStatusChanged(String provider, int status, Bundle extras) {
+            mEventBus.post(new OBDDataMessage(OBDDataMessage.CONTENT_FLAG, "onStatusChanged"));
             Log.d(TAG, "onStatusChanged: " + status);
         }
 
         @Override
         public void onProviderEnabled(String provider) {
+            mEventBus.post(new OBDDataMessage(OBDDataMessage.CONTENT_FLAG, "onProviderEnabled"));
             Log.d(TAG, "onProviderEnabled: " + provider);
         }
 
         @Override
         public void onProviderDisabled(String provider) {
+            mEventBus.post(new OBDDataMessage(OBDDataMessage.CONTENT_FLAG, "onProviderDisabled"));
             Log.d(TAG, "onProviderDisabled: " + provider);
         }
     };
